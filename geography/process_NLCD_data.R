13b1a9334c75e1f078f2b77d593063f0f2b4f433
Xiaodan Xu <arielinseu@gmail.com>
2024-01-04 17:10:43 -0800

    process hawaii nlcd by island images

--- geography/process_NLCD_data.R
+++ geography/process_NLCD_data.R
@@ -21,6 +21,8 @@ library(tigris)
 library(stringr)
 library(lwgeom) # to calculate area
 library(tidyr) # to use spread func
+library(raster) # for HI ige file
+library(stars)
 
 sf_use_s2(FALSE)
 
@@ -72,23 +74,23 @@ nlcd_data_by_tract<- nlcd_data_centroid_df %>%
 write.csv(nlcd_data_by_tract, file.path('Land_use', cleandir, 'tract_level_land_use_no_ak.csv'))
 
 # fill land use for AK with old NLCD
-ak_hi_nlcd_path <- 'emiss_shp2017/NLCD/CONUS_AK_NLCD_2011_500m_WGS.shp'
-ak_hi_nlcd_data <- st_read(file.path('Land_use', datadir, ak_hi_nlcd_path))
+ak_nlcd_path <- 'emiss_shp2017/NLCD/CONUS_AK_NLCD_2011_500m_WGS.shp'
+ak_nlcd_data <- st_read(file.path('Land_use', datadir, ak_nlcd_path))
 
-ak_hi_nlcd_data$area <- st_area(ak_hi_nlcd_data)
-ak_nlcd_data_centroid <- st_centroid(ak_hi_nlcd_data)
+ak_nlcd_data$area <- st_area(ak_nlcd_data)
+ak_nlcd_data_centroid <- st_centroid(ak_nlcd_data)
 ak_nlcd_data_sample <- head(ak_nlcd_data_centroid, 1000)
 plot(ak_nlcd_data_sample[, 'GRIDCODE'])
 # get ak boundary
 states <- tigris::states(cb = TRUE)
-ak_hi_boundary <- states %>% dplyr::filter(STUSPS == 'AK' | STUSPS == 'HI')
-plot(st_geometry(ak_hi_boundary))
-ak_hi_boundary <- st_transform(ak_hi_boundary, 4326) # re-project data
+ak_boundary <- states %>% dplyr::filter(STUSPS == 'AK')
+plot(st_geometry(ak_boundary))
+ak_boundary <- st_transform(ak_boundary, 4326) # re-project data
 
 # using 'st_intersects', which gives a boolean for if points within polygon
-ak_nlcd_data_centroid <- ak_nlcd_data_centroid[which(unlist(st_intersects(ak_nlcd_data_centroid, ak_hi_boundary)) == 1),] 
+ak_nlcd_data_centroid <- ak_nlcd_data_centroid[which(unlist(st_intersects(ak_nlcd_data_centroid, ak_boundary)) == 1),] 
 
-plot(st_geometry(ak_hi_boundary[ak_hi_boundary$STUSPS == 'AK',]))
+plot(st_geometry(ak_boundary[ak_boundary$STUSPS == 'AK',]))
 plot(ak_nlcd_data_centroid[1:1000, 'GRIDCODE'], add = TRUE)
 #ak_nlcd_data_sample <- st_intersection(ak_nlcd_data_sample, ak_hi_boundary)
 
@@ -96,18 +98,18 @@ plot(ak_nlcd_data_centroid[1:1000, 'GRIDCODE'], add = TRUE)
 ak_nlcd_data_centroid <- ak_nlcd_data_centroid %>% select(ID, GRIDCODE, STATEFP, area, geometry)
 
 # assign NLCD data to ak, hi tracts
-ak_hi_tracts <- us_census_tract %>% dplyr::filter(STATEFP %in% c('02', '15'))
-plot(st_geometry(ak_hi_tracts))
+ak_tracts <- us_census_tract %>% dplyr::filter(STATEFP %in% c('02'))
+plot(st_geometry(ak_tracts))
 
-ak_nlcd_data_centroid <- st_join(ak_nlcd_data_centroid, ak_hi_tracts,
+ak_nlcd_data_centroid <- st_join(ak_nlcd_data_centroid, ak_tracts,
                               join = st_nearest_feature, left = TRUE)
 
 ak_nlcd_data_centroid_df <- ak_nlcd_data_centroid %>% st_drop_geometry()
-write.csv(ak_nlcd_data_centroid_df, file.path('Land_use', cleandir, 'assigned_nlcd_to_tract_ak_hi.csv'))
+write.csv(ak_nlcd_data_centroid_df, file.path('Land_use', cleandir, 'assigned_nlcd_to_tract_ak.csv'))
 
-nlcd_data_centroid_sample <- head(ak_nlcd_data_centroid_df, 1000)          
+nlcd_data_centroid_sample <- head(ak_nlcd_data_centroid, 1000)          
 selected_tracts <- unique(nlcd_data_centroid_sample$GEOID)
-selected_tracts_sf <- ak_hi_tracts[ak_hi_tracts$GEOID %in% selected_tracts,]
+selected_tracts_sf <- ak_tracts[ak_tracts$GEOID %in% selected_tracts,]
 plot(st_geometry(selected_tracts_sf))
 plot(nlcd_data_centroid_sample[, 'GEOID'], add= TRUE)
 
@@ -119,15 +121,75 @@ plot(nlcd_data_centroid_sample[, 'GEOID'], add= TRUE)
 nlcd_data_centroid_sample_df <- nlcd_data_centroid_sample %>% st_drop_geometry()
 
 ak_nlcd_data_centroid_df <- ak_nlcd_data_centroid_df %>% 
-  dplyr::mutate(area = as.numeric(area)) # unit in m^2
+  dplyr::mutate(area = as.numeric(area)) %>% # unit in m^2
+ dplyr::select(-ID) # unit in m^2
 
 ak_nlcd_data_by_tract<- ak_nlcd_data_centroid_df %>%
   tidyr::pivot_wider(names_from = GRIDCODE, values_from = area, 
                      values_fn = sum, values_fill = 0)
 
-write.csv(ak_nlcd_data_by_tract, file.path('Land_use', cleandir, 'tract_level_land_use_ak_hi.csv'))
+write.csv(ak_nlcd_data_by_tract, file.path('Land_use', cleandir, 'tract_level_land_use_ak.csv'))
 
+
+# process Hawaii land cover
+hi_nlcd_path <- 'hi_hawaii_2010_ccap_hr_land_cover20150120/hi_hawaii_2010_ccap_hr_land_cover20150120.img'
+# hi_nlcd_data <- raster(file.path('Land_use', datadir, hi_nlcd_path))
+# plot(hi_nlcd_data)
+
+#tif = system.file(file.path('Land_use', datadir, hi_nlcd_path), package = "stars")
+hi_nlcd_data = read_stars(file.path('Land_use', datadir, hi_nlcd_path))
+hi_nlcd_data_sf <- st_as_sf(hi_nlcd_data, as_points = FALSE, merge = TRUE)
+st_write(hi_nlcd_data_sf, file.path('Land_use', datadir, 'HI_NLCD_2010_P1.geojson'))
+hi_nlcd_data_sample <- head(hi_nlcd_data_sf, 1000)
+plot(hi_nlcd_data_sample)
+
+hi_nlcd_path <- 'hi_hawaii_2010_ccap_hr_land_cover20150120/hi_hawaii_2010_ccap_hr_land_cover20150120.img'
+# hi_nlcd_data <- raster(file.path('Land_use', datadir, hi_nlcd_path))
+# plot(hi_nlcd_data)
+
+#tif = system.file(file.path('Land_use', datadir, hi_nlcd_path), package = "stars")
+hi_nlcd_path_p2 <- 'hi_hawaii_2010_ccap_hr_land_cover20150120/hi_oahu_2011_ccap_hr_land_cover20140619.img'
+hi_nlcd_data_p2 = read_stars(file.path('Land_use', datadir, hi_nlcd_path_p2))
+hi_nlcd_data_p2_sf <- st_as_sf(hi_nlcd_data_p2, as_points = FALSE, merge = TRUE)
+hi_nlcd_data_sample <- head(hi_nlcd_data_p2_sf, 1000)
+plot(hi_nlcd_data_sample)
+st_write(hi_nlcd_data_p2_sf, file.path('Land_use', datadir, 'HI_NLCD_2010_oahu.geojson'))
+
+hi_nlcd_path_p3 <- 'hi_hawaii_2010_ccap_hr_land_cover20150120/hi_niihau_2010_ccap_hr_land_cover20140930.img'
+hi_nlcd_data_p3 = read_stars(file.path('Land_use', datadir, hi_nlcd_path_p3))
+hi_nlcd_data_p3_sf <- st_as_sf(hi_nlcd_data_p3, as_points = FALSE, merge = TRUE)
+hi_nlcd_data_sample <- head(hi_nlcd_data_p3_sf, 1000)
+plot(hi_nlcd_data_sample)
+st_write(hi_nlcd_data_p3_sf, file.path('Land_use', datadir, 'HI_NLCD_2010_niihau.geojson'))
+
+hi_nlcd_path_p4 <- 'hi_hawaii_2010_ccap_hr_land_cover20150120/hi_molokai_2010_ccap_hr_land_cover20150102.img'
+hi_nlcd_data_p4 = read_stars(file.path('Land_use', datadir, hi_nlcd_path_p4))
+hi_nlcd_data_p4_sf <- st_as_sf(hi_nlcd_data_p4, as_points = FALSE, merge = TRUE)
+hi_nlcd_data_sample <- head(hi_nlcd_data_p4_sf, 1000)
+plot(hi_nlcd_data_sample)
+st_write(hi_nlcd_data_p4_sf, file.path('Land_use', datadir, 'HI_NLCD_2010_molokai.geojson'))
+
+hi_nlcd_path_p5 <- 'hi_hawaii_2010_ccap_hr_land_cover20150120/hi_maui_2010_ccap_hr_land_cover_20150213.img'
+hi_nlcd_data_p5 = read_stars(file.path('Land_use', datadir, hi_nlcd_path_p5))
+hi_nlcd_data_p5_sf <- st_as_sf(hi_nlcd_data_p5, as_points = FALSE, merge = TRUE)
+hi_nlcd_data_sample <- head(hi_nlcd_data_p5_sf, 1000)
+plot(hi_nlcd_data_sample)
+st_write(hi_nlcd_data_p5_sf, file.path('Land_use', datadir, 'HI_NLCD_2010_maui.geojson'))
+
+hi_nlcd_path_p6 <- 'hi_hawaii_2010_ccap_hr_land_cover20150120/hi_lanai_2011_ccap_hr_land_cover_20141204.img'
+hi_nlcd_data_p6 = read_stars(file.path('Land_use', datadir, hi_nlcd_path_p6))
+hi_nlcd_data_p6_sf <- st_as_sf(hi_nlcd_data_p6, as_points = FALSE, merge = TRUE)
+hi_nlcd_data_sample <- head(hi_nlcd_data_p6_sf, 1000)
+plot(hi_nlcd_data_sample)
+st_write(hi_nlcd_data_p6_sf, file.path('Land_use', datadir, 'HI_NLCD_2010_lanai.geojson'))
+
+hi_nlcd_path_p7 <- 'hi_hawaii_2010_ccap_hr_land_cover20150120/hi_kauai_2010_ccap_hr_land_cover20140929.img'
+hi_nlcd_data_p7 = read_stars(file.path('Land_use', datadir, hi_nlcd_path_p7))
+hi_nlcd_data_p7_sf <- st_as_sf(hi_nlcd_data_p7, as_points = FALSE, merge = TRUE)
+hi_nlcd_data_sample <- head(hi_nlcd_data_p7_sf, 1000)
+plot(hi_nlcd_data_sample)
+st_write(hi_nlcd_data_p7_sf, file.path('Land_use', datadir, 'HI_NLCD_2010_kauai.geojson'))
 # check final results
-us_census_tract_no_nlcd <- us_census_tract %>% 
-  dplyr::filter(!GEOID %in%  unique(nlcd_data_centroid_df$GEOID))
-plot(st_geometry(us_census_tract_no_nlcd))
+# us_census_tract_no_nlcd <- us_census_tract %>% 
+#   dplyr::filter(!GEOID %in%  unique(nlcd_data_centroid_df$GEOID))
+# plot(st_geometry(us_census_tract_no_nlcd))
